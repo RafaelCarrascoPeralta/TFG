4760e105d76addbe68125c35c956c311aef7542a
RafaelCarrascoPeralta <117440215+RafaelCarrascoPeralta@users.noreply.github.com>
2024-06-04 22:19:52 +0200

    Update actualizar_equipo.php

--- federacion/admin/actualizar_equipo.php
+++ federacion/admin/actualizar_equipo.php
@@ -3,7 +3,7 @@
 ?>
     <div id=admin>
                         <a href="index.php"><img src=".\imagen\admin.png" width="25px">Admin</a>
-                        <form id="cerrar_sesion" action="/federacion/index.php" method="post" enctype="multipart/form-data">
+                        <form id="cerrar_sesion" action="index.php" method="post" enctype="multipart/form-data">
                             <input type="submit" name="enviar" value="Cerrar Sesion">
                         </form>
               
