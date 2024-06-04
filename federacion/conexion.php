b83f8e78f591ba050e1cafe047cb3843df53c9a4
RafaelCarrascoPeralta <117440215+RafaelCarrascoPeralta@users.noreply.github.com>
2024-06-05 00:13:49 +0200

    Update conexion.php

--- federacion/conexion.php
+++ federacion/conexion.php
@@ -2,10 +2,11 @@
     $servidor="mysql-service";
     $usuario="root";
     $passw="admin";
+    $basededatos = "federacion"; // Añadí el nombre de la base de datos aquí
 
     $conexion = new mysqli($servidor, $usuario, $passw, $basededatos);
 
     if ($conexion->connect_error) {
-        die("La conexión fallo: 0" . $conexion->$connect_error);
+        die("La conexión falló: " . $conexion->connect_error);
     }
 ?>
