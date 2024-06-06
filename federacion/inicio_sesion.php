ee0ed48d927c6874229e5ab2aaaab9e424b8c2d8
RafaelCarrascoPeralta <117440215+RafaelCarrascoPeralta@users.noreply.github.com>
2024-06-06 19:41:58 +0200

    Update inicio_sesion.php

--- federacion/inicio_sesion.php
+++ federacion/inicio_sesion.php
@@ -1,5 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="./css/estilos.css">
+        <title>Federacion Andaluza de Baloncesto</title>
+    </head>
+    <body>
+        <header>
+            <div id="logo_cabecera">
+                <img src="./imagen/Logo-FAB.png" width="250px">
+            </div>
+            <div id="navegador">
+                <div id="nav_menu">
+                    <a href="index.php"><img src="./imagen/casa.png" width="25px">Inicio</a>
+                </div>
+                <div id="nav_menu">
+                    <a href="equipos.php"><img src="./imagen/usuario.png" width="25px">Equipos</a>
+                </div>
+                <div id="nav_menu">
+                    <a href="partidos.php"><img src="./imagen/calendario.png" width="25px">Partidos</a>
+                </div>
+                <div id="nav_menu">
+                    <a href="clasificacion.php"><img src="./imagen/clasificacion.png" width="25px">Clasificacion</a>
+                </div>
+            </div>
+        </header>
+        <main>
+
 <?php
-include "header.php";
 
 $usu = $_POST['usuario'];
 $contraseña = $_POST['contraseña'];
@@ -65,6 +94,31 @@ if ($stmt = $conexion->prepare($sql_admin)) {
 // Cerrar la conexión
 $conexion->close();
 
-include "footer.php";
-
 ?>
+
+        </main>
+        <footer>
+            <div id="pie_pagina">
+                <div id="datos">
+                    <address>
+                        <p>__________________________________________</p>
+                        <p>Avenida de la Constitución 20</p>
+                        <p>Edif la Pirámide Local 6C, 18012 Granada</p>
+                        <p>958 21 54 93</p>
+                        <p>javier.simon@baloncestoandalucia.org</p>
+                        <p>__________________________________________</p>
+                    </address>
+                </div>
+                <div id="instituto">
+                    <p>FEDERACION GRANADINA DE BALONCESTO</p>
+                    <img src="./imagen/Logo-FAB_footer.png" width="190px">
+                </div>
+                <div id="redes">
+                    <a href="https://www.instagram.com/andaluzabaloncesto/?hl=es" target="_blank"><img src="./imagen/instagram.png" width="50px"></a>
+                    <a href="https://www.facebook.com/andaluzabaloncesto" target="_blank"><img src="./imagen/facebook.png" width="50px"></a>
+                    <a href="https://www.youtube.com/@andaluzabaloncesto/featured" target="_blank"><img src="./imagen/youtube.png" width="50px"></a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
