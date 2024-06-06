1fa2f1458909ae425c44e99af3b74e0820ae9d6a
RafaelCarrascoPeralta <117440215+RafaelCarrascoPeralta@users.noreply.github.com>
2024-06-06 19:54:00 +0200

    Update baja_equipo.php

--- federacion/admin/baja_equipo.php
+++ federacion/admin/baja_equipo.php
@@ -1,37 +1,32 @@
 <?php
-    include "header.php";
+// Manejar la lógica de eliminación antes de incluir cualquier HTML
 
-    $basededatos="federacion";
+$basededatos = "federacion";
+include "conexion.php";
 
-    include "conexion.php";
+$equipoborrar = $_GET['nombre_equipo'];
 
+// Borrar jugadores
+$borrar_jugador = "DELETE FROM jugadores WHERE nombre_equipo='$equipoborrar'";
+mysqli_query($conexion, $borrar_jugador);
+
+// Borrar partidos
+$borrar_partido = "DELETE FROM partidos WHERE nombre_equipo='$equipoborrar' OR visitante='$equipoborrar'";
+mysqli_query($conexion, $borrar_partido);
+
+// Borrar equipo
+$borrar_equipo = "DELETE FROM equipos WHERE nombre_equipo='$equipoborrar'";
+mysqli_query($conexion, $borrar_equipo);
+
+// Redireccionar después de la eliminación
+header("Location: borrar_equipos.php");
+exit();
 ?>
 
-<div id=contenido>
+<?php include "header.php"; ?>
 
-<?php
-
-    $equipoborrar=$_GET['nombre_equipo'];
-    // Borrar jugadores
-    $borrar_jugador = "DELETE FROM jugadores WHERE nombre_equipo='$equipoborrar'";
-    mysqli_query($conexion, $borrar_jugador);
-
-    // Borrar partidos
-    $borrar_partido = "DELETE FROM partidos WHERE nombre_equipo='$equipoborrar' OR visitante='$equipoborrar'";
-    mysqli_query($conexion, $borrar_partido);
-
-    // Borrar equipo
-    $borrar_equipo = "DELETE FROM equipos WHERE nombre_equipo='$equipoborrar'";
-    mysqli_query($conexion, $borrar_equipo);
-    header("Location:borrar_equipos.php");
-    
-
-?>
-
-
+<div id="contenido">
+    <p>El equipo ha sido eliminado.</p>
 </div>
 
-<?php
-    include "footer.php";
-
-?>
+<?php include "footer.php"; ?>
