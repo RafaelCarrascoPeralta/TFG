1d4e9e72c9076c79f8e812e3295d61b5889d75b1
RafaelCarrascoPeralta <117440215+RafaelCarrascoPeralta@users.noreply.github.com>
2024-06-11 16:43:02 +0200

    Update equipo_actualizado.php

--- federacion/admin/equipo_actualizado.php
+++ federacion/admin/equipo_actualizado.php
@@ -13,84 +13,96 @@
     $basededatos="federacion";
 
     include "conexion.php";
+
 ?>
 
 <div id=admin>
-    <a href="#"><img src=".\imagen\admin.png" width="25px">Admin</a>
-    <form id="cerrar_sesion" action="../index.php" method="post" enctype="multipart/form-data">
-        <input type="submit" name="enviar" value="Cerrar Sesion">
-    </form>
-</div>
+                        <a href="#"><img src=".\imagen\admin.png" width="25px">Admin</a>
+                        <form id="cerrar_sesion" action="../index.php" method="post" enctype="multipart/form-data">
+                            <input type="submit" name="enviar" value="Cerrar Sesion">
+                        </form>
+              
+    </div>
     
+
 <div id=content>
     <div id=mensaje>
-        <?php
-            if ($nombrearchivo == "") {
-                $sql_modificar = "SELECT * FROM equipos WHERE nombre_equipo='$nombre'";
-                $resultado = mysqli_query($conexion, $sql_modificar);
+
+<?php
+
+
+        if ($nombrearchivo == ""){
+
+            $sql_modificar = "SELECT * FROM equipos WHERE nombre_equipo='$nombre'";
+            $resultado = mysqli_query($conexion, $sql_modificar);
 
                 if ($registro = mysqli_fetch_row($resultado)) {
-                    $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='" . $registro['7'] . "' where nombre_equipo='$nombre'";
-                    $resultado = mysqli_query($conexion, $modificar);
-
-                    if ($resultado) {
-                        echo "Los datos se actualizaron correctamente";
-                    } else {
-                        echo "Error al actualizar los datos: " . mysqli_error($conexion);
-                    }
-                }
-            } else {
-                $directoriosubida = "/var/www/html/imagen/";
-                $max_file_size = "5120000";
-                $extensionesvalidas = array("jpg", "png", "gif");
-                $nombrecompleto = "";
-
-                if (isset($_FILES['imagen'])) {
-                    $errores = 0;
-                    $nombrearchivo = $_FILES['imagen']['name'];
-                    $filesize = $_FILES['imagen']['size'];
-                    $directoriotemp = $_FILES['imagen']['tmp_name'];
-                    $tipoarchivo = $_FILES['imagen']['type'];
-                    $arrayarchivo = pathinfo($nombrearchivo);
-                    $extension = $arrayarchivo['extension'];
-
-                    if (!in_array($extension, $extensionesvalidas)) {
-                        echo "La extensión no es válida";
-                        $errores = 1;
-                    }
-
-                    if ($filesize > $max_file_size) {
-                        echo "La imagen excede el máximo tamaño permitido";
-                        $errores = 1;
-                    }
-
-                    if ($errores == 0) {
-                        $nombreimagen = $directoriosubida . $nombrearchivo;
-                        if (move_uploaded_file($directoriotemp, $nombreimagen)) {
-                            // Copiar la imagen a /admin/imagen
-                            $adminImagenDir = "/var/www/html/admin/imagen/" . $nombrearchivo;
-                            if (!copy($nombreimagen, $adminImagenDir)) {
-                                echo "Error al copiar la imagen al directorio de administración.";
-                            }
-                        } else {
-                            echo "Error al mover la imagen al directorio de subida.";
-                        }
-                    }
-                }
-
-                $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='$nombreimagen' where nombre_equipo='$nombre'";
+                $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='" . $registro['7'] . "' where nombre_equipo='$nombre'";
                 $resultado = mysqli_query($conexion, $modificar);
-
+                
                 if ($resultado) {
                     echo "Los datos se actualizaron correctamente";
                 } else {
                     echo "Error al actualizar los datos: " . mysqli_error($conexion);
                 }
             }
-        ?>
+
+        } else {
+
+
+            $directoriosubida = "/var/www/html/imagen/";
+            $max_file_size = "5120000";
+            $extensionesvalidas = array("jpg", "png", "gif");
+
+            $nombrecompleto = "";
+
+        if (isset($_FILES['imagen'])) {
+            $errores = 0;
+            $nombrearchivo = $_FILES['imagen']['name'];
+            $filesize = $_FILES['imagen']['size'];
+            $directoriotemp = $_FILES['imagen']['tmp_name'];
+            $tipoarchivo = $_FILES['imagen']['type'];
+            $arrayarchivo = pathinfo($nombrearchivo);
+            $extension = $arrayarchivo['extension'];
+
+            if (!in_array($extension, $extensionesvalidas)) {
+                echo "La extensión no es válida";
+                $errores = 1;
+            }
+
+            if ($filesize > $max_file_size) {
+                echo "La imagen excede el máximo tamaño permitido";
+                $errores = 1;
+            }
+
+            if ($errores == 0) {
+                $nombreimagen = $directoriosubida . $nombrearchivo;
+                move_uploaded_file($directoriotemp, $nombreimagen);
+                directoriosubida2 = "/var/www/html/admin/imagen/";
+                $nombreimagen2 = $directoriosubida2 . $nombrearchivo;
+                copy($nombreimagen, $nombreimagen2);
+            }
+
+        }
+
+    $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='$nombreimagen' where nombre_equipo='$nombre'";
+    $resultado = mysqli_query($conexion, $modificar);
+    
+    if ($resultado) {
+        echo "Los datos se actualizaron correctamente";
+    } else {
+        echo "Error al actualizar los datos: " . mysqli_error($conexion);
+    }         
+
+}
+
+?>
+
     </div>
+
 </div>
 
 <?php
     include "footer.php";
+
 ?>
