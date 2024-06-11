5b2e768dbf6b94cef0e930827d46e9bb13834ec5
RafaelCarrascoPeralta <117440215+RafaelCarrascoPeralta@users.noreply.github.com>
2024-06-11 15:56:56 +0200

    Update equipo_actualizado.php

--- federacion/admin/equipo_actualizado.php
+++ federacion/admin/equipo_actualizado.php
@@ -10,34 +10,71 @@
     $correo = $_POST['correo'];
     $nombrearchivo = $_FILES['imagen']['name'];
 
-    $basededatos="federacion";
+    $basededatos = "federacion";
 
     include "conexion.php";
-
 ?>
 
 <div id=admin>
-                        <a href="#"><img src=".\imagen\admin.png" width="25px">Admin</a>
-                        <form id="cerrar_sesion" action="../index.php" method="post" enctype="multipart/form-data">
-                            <input type="submit" name="enviar" value="Cerrar Sesion">
-                        </form>
-              
-    </div>
+    <a href="#"><img src=".\imagen\admin.png" width="25px">Admin</a>
+    <form id="cerrar_sesion" action="../index.php" method="post" enctype="multipart/form-data">
+        <input type="submit" name="enviar" value="Cerrar Sesion">
+    </form>
+</div>
     
-
 <div id=content>
     <div id=mensaje>
-
-<?php
-
-
-        if ($nombrearchivo == ""){
-
-            $sql_modificar = "SELECT * FROM equipos WHERE nombre_equipo='$nombre'";
-            $resultado = mysqli_query($conexion, $sql_modificar);
+        <?php
+            if ($nombrearchivo == "") {
+                $sql_modificar = "SELECT * FROM equipos WHERE nombre_equipo='$nombre'";
+                $resultado = mysqli_query($conexion, $sql_modificar);
 
                 if ($registro = mysqli_fetch_row($resultado)) {
-                $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='" . $registro['7'] . "' where nombre_equipo='$nombre'";
+                    $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='" . $registro['7'] . "' WHERE nombre_equipo='$nombre'";
+                    $resultado = mysqli_query($conexion, $modificar);
+                    
+                    if ($resultado) {
+                        echo "Los datos se actualizaron correctamente";
+                    } else {
+                        echo "Error al actualizar los datos: " . mysqli_error($conexion);
+                    }
+                }
+            } else {
+                $directorio_subida1 = "/var/www/html/imagen/";
+                $directorio_subida2 = "/var/www/html/admin/imagen/";
+                $max_file_size = 5120000;
+                $extensiones_validas = array("jpg", "png", "gif");
+
+                if (isset($_FILES['imagen'])) {
+                    $errores = 0;
+                    $nombrearchivo = $_FILES['imagen']['name'];
+                    $filesize = $_FILES['imagen']['size'];
+                    $directorio_temp = $_FILES['imagen']['tmp_name'];
+                    $tipoarchivo = $_FILES['imagen']['type'];
+                    $arrayarchivo = pathinfo($nombrearchivo);
+                    $extension = $arrayarchivo['extension'];
+
+                    if (!in_array($extension, $extensiones_validas)) {
+                        echo "La extensión no es válida";
+                        $errores = 1;
+                    }
+
+                    if ($filesize > $max_file_size) {
+                        echo "La imagen excede el máximo tamaño permitido";
+                        $errores = 1;
+                    }
+
+                    if ($errores == 0) {
+                        $nombre_imagen1 = $directorio_subida1 . $nombrearchivo;
+                        $nombre_imagen2 = $directorio_subida2 . $nombrearchivo;
+                        
+                        if (move_uploaded_file($directorio_temp, $nombre_imagen1)) {
+                            copy($nombre_imagen1, $nombre_imagen2);
+                        }
+                    }
+                }
+
+                $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='$nombre_imagen1' WHERE nombre_equipo='$nombre'";
                 $resultado = mysqli_query($conexion, $modificar);
                 
                 if ($resultado) {
@@ -46,59 +83,11 @@
                     echo "Error al actualizar los datos: " . mysqli_error($conexion);
                 }
             }
-
-        } else {
-
-
-            $directoriosubida = "imagen/";
-            $max_file_size = "5120000";
-            $extensionesvalidas = array("jpg", "png", "gif");
-
-            $nombrecompleto = "";
-
-        if (isset($_FILES['imagen'])) {
-            $errores = 0;
-            $nombrearchivo = $_FILES['imagen']['name'];
-            $filesize = $_FILES['imagen']['size'];
-            $directoriotemp = $_FILES['imagen']['tmp_name'];
-            $tipoarchivo = $_FILES['imagen']['type'];
-            $arrayarchivo = pathinfo($nombrearchivo);
-            $extension = $arrayarchivo['extension'];
-
-            if (!in_array($extension, $extensionesvalidas)) {
-                echo "La extensión no es válida";
-                $errores = 1;
-            }
-
-            if ($filesize > $max_file_size) {
-                echo "La imagen excede el máximo tamaño permitido";
-                $errores = 1;
-            }
-
-            if ($errores == 0) {
-                $nombreimagen = $directoriosubida . $nombrearchivo;
-                move_uploaded_file($directoriotemp, $nombreimagen);
-            }
-        }
-
-    $modificar = "UPDATE equipos SET localidad='$localidad', dia_partido='$dia', hora_partido='$hora', color_equipacion='$color', lugar_partido='$lugar', correo='$correo', logo='$nombreimagen' where nombre_equipo='$nombre'";
-    $resultado = mysqli_query($conexion, $modificar);
-    
-    if ($resultado) {
-        echo "Los datos se actualizaron correctamente";
-    } else {
-        echo "Error al actualizar los datos: " . mysqli_error($conexion);
-    }         
-
-}
-
-?>
-
+        ?>
     </div>
-
 </div>
 
 <?php
     include "footer.php";
-
 ?>
+
